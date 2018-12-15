74e7d0161b357a2f0c1ed20adb3576a388b545d9
Ezzat Suhaime <ezzatisawesome@gmail.com>
2018-12-14 20:55:25 -0600

    Worked on mag test

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-from MPU9250.MPU9250 import MPU9250
+import MPU9250
 import smbus
 import time
 import math
+import sys
 
-sensor = mpu.mpu9250() # default pin is AIN0
+mpu9250 = MPU9250.MPU9250()
 
+try:
+	while True:
+		mag = mpu9250.readMagnet()
+		print(mag['x'])
+		print(mag['y'])
+		print(mag['z'], '\n')
+		time.sleep(1)
+
+except KeyboardInterrupt:
+	sys.exit
 
 
