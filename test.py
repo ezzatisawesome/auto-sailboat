ec0fa243186b58688ed1e10493fcf0234bdfe705
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2018-12-14 18:38:07 -0600

    Worked on library

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 from MPU9250.MPU9250 import MPU9250
-import I2CMPU.python_i2c_mpu9250 as mpu
 import smbus
 import time
 import math
 
 sensor = mpu.mpu9250() # default pin is AIN0
-while True:
-    value = sensor.read_all()
-    print(value)
-    time.sleep(0.2)
+
+
 
