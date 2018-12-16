9c3155cae76f98dde87a471b814e942f6cfa4eb8
Ezzat Suhaime <ezzatisawesome@gmail.com>
2018-12-15 22:26:09 -0600

    Worked on a program that outputed raw mag values(unfiltered) to heading

--- test_degrees.py
+++ test_degrees.py
@@ -1,5 +1,5 @@
 import MPU9250
-import smbus 
+import smbus
 import time
 import math
 import sys
@@ -8,5 +8,17 @@ import sys
 mpu9250 = MPU9250.MPU9250()
 
 #function for converting raw magnetometer data to degrees
-def raw_to_degrees():
-	heading = math.atan2(y, x) * 180/math.pi
+def raw_to_degrees(x, y):
+	return math.atan2(y, x) * 180/math.pi
+
+try:
+	while True:
+		mag = mpu9250.readMagnet()
+		x = mag['x']  # the z
+		y = mag['y']  # the y
+		z = mag['z']  # the z
+		print(raw_to_degrees(x, y)) #get input into raw_to_degrees function
+		time.sleep(1)
+
+except KeyboardInterrupt:
+	sys.exit
