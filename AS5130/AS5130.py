51e805c9c899bc38db290e16ef83638f1d35bfbb
Ezzat Suhaime <ezzatisawesome@gmail.com>
2018-12-21 17:27:21 -0600

    Update AS5130.py

--- AS5130/AS5130.py
+++ AS5130/AS5130.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 import math
 
-IO.setwarnings(False) #got from random website
-IO.setmode(IO.BCM) #got from random website
-IO.setup(19,IO.IN) #got from random website
+GIO.setwarnings(False) #got from random website
+GIO.setmode(IO.BCM) #got from random website
+GIO.setup(19,IO.IN) #got from random website
 
-p = IO.PWM(output channel , frequency of PWM signal) #got from random website
+p = GIO.PWM(output channel , frequency of PWM signal) #got from random website
 
 angle = 0 #angle value
 angle = (365/256)*(257(ton/(ton*toff)-1) #equation from datasheet of AS5130 to convert pwn to degree value (HAVE TO DEFINE WHAT "ton" and "toff" are)
