b9a497a2d7c8c80697c3a5fe5a7dcef48a56ddca
Ezzat Suhaime <ezzatisawesome@gmail.com>
2018-12-31 17:11:12 -0600

    Worked on IMU_CALIB

--- test.py
+++ test.py
@@ -14,15 +14,15 @@ try:
         magx = mag['x'] #x mag value
         magy = mag['y'] #y mag value
         magz = mag['z'] #z mag value
-        comp_heading = IMU_CALIB.tilt_comp(magx, magy, magz)
 
         accelx = accel['x'] #x mag value
         accely = accel['y'] #y mag value
         accelz = accel['z'] #z mag value
-        pitch = IMU_CALIB.pitch(accelx, accely, accelz) #creating instance of pitch function in IMU_CALIB lib
-        roll = IMU_CALIB.roll(accelx, accely, accelz) #creating instance of roll function in IMU_CALIB lib
-        
-        #print("{:>.4f}  {:>.4f}  {:>.4f}" .format(accelx, accely, accelz)) #printing accelerometer raw values
+
+        pitch = IMU_CALIB.pitch(accelx, accely, accelz) #creating instance of pitch method in IMU_CALIB lib
+        roll = IMU_CALIB.roll(accelx, accely, accelz) #creating instance of roll method in IMU_CALIB lib
+
+        comp_heading = IMU_CALIB.mag_tilt_comp(magx, magy, magz) #creating instance of tilt compensation method in IMU_CALIB lib
 
         print("{:>.4f} {:>.4f}" .format(pitch, roll))
         print("{:>.4f}" .format(comp_heading))
