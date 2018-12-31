8408a4f6a4319fd8f0fca82f4f08c755ea23efa7
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2018-12-31 16:33:43 -0600

    Worked on IMU_CALIB

--- IMU_CALIB.py
+++ IMU_CALIB.py
@@ -4,14 +4,14 @@ class IMU_CALIB:
     def __init__(self):
         pass
 
-    def pitch(self, accelx, accely, accelz): # method to get pitch from raw accelerometer values
+    def pitch(self, accelx, accely, accelz): #method to get pitch from raw accelerometer values
 
         accXnorm = accelx/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
-        self.calcpitch = math.asin(accXnorm) # converting to pitch values in radians
+        self.calcpitch = math.asin(accXnorm) #converting to pitch values in radians
 
         return(self.calcpitch)
 
-    def roll(self, accelx, accely, accelz): # method to get roll from raw accelerometer values
+    def roll(self, accelx, accely, accelz): #method to get roll from raw accelerometer values
 
         accYnorm = accely/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel y values
         self.calcroll = -1*math.asin(accYnorm/math.cos(self.calcpitch)) #converting to roll values in radians
@@ -21,10 +21,10 @@ class IMU_CALIB:
 
     def tilt_comp(self, magx, magy, magz):
 
-        self.magXcomp = magx*math.cos(self.pitch) + magz*math.sin(self.pitch) # calucluate compensated magx values
-        self.magYcomp = magx*math.sin(self.roll)*math.sin(self.pitch) + magy*math.cos(self.roll) - magz*math.sin(self.roll)*math.cos(self.pitch) # calculate compensated magy values
+        magXcomp = magx*math.cos(self.pitch) + magz*math.sin(self.pitch) # calucluate compensated magx values
+        magYcomp = magx*math.sin(self.roll)*math.sin(self.pitch) + magy*math.cos(self.roll) - magz*math.sin(self.roll)*math.cos(self.pitch) # calculate compensated magy values
 
-        self.comp_heading = (180 * math.atan2(self.magYcomp, self.magXcomp)) / math.pi
+        self.comp_heading = 180 * math.atan2(magYcomp, magXcomp) / math.pi
 
         return(self.comp_heading)
 
--- test.py
+++ test.py
@@ -8,26 +8,26 @@ IMU_CALIB = IMU_CALIB.IMU_CALIB()
 
 try:
      while True:
-        mag = mpu9250.readMagnet()
-        accel = mpu9250.readAccel()
+        mag = mpu9250.readMagnet() #read magnetometer
+        accel = mpu9250.readAccel() #read accelerometer
         
-        magx = mag['x'] # x mag value
-        magy = mag['y'] # y mag value
-        magz = mag['z'] # z mag value
+        magx = mag['x'] #x mag value
+        magy = mag['y'] #y mag value
+        magz = mag['z'] #z mag value
+        comp_heading = IMU_CALIB.tilt_comp(magx, magy, magz)
 
-        accelx = accel['x'] # x mag value
-        accely = accel['y'] # y mag value
-        accelz = accel['z'] # z mag value
-
-        #print("{:>.4f}  {:>.4f}  {:>.4f}" .format(accelx, accely, accelz))
-
-
-        pitch = IMU_CALIB.pitch(accelx, accely, accelz)
-        roll = IMU_CALIB.roll(accelx, accely, accelz)
+        accelx = accel['x'] #x mag value
+        accely = accel['y'] #y mag value
+        accelz = accel['z'] #z mag value
+        pitch = IMU_CALIB.pitch(accelx, accely, accelz) #creating instance of pitch function in IMU_CALIB lib
+        roll = IMU_CALIB.roll(accelx, accely, accelz) #creating instance of roll function in IMU_CALIB lib
+        
+        #print("{:>.4f}  {:>.4f}  {:>.4f}" .format(accelx, accely, accelz)) #printing accelerometer raw values
 
         print("{:>.4f} {:>.4f}" .format(pitch, roll))
+        print("{:>.4f}" .format(comp_heading))
 
-        time.sleep(2) # repeat two every seconds
+        time.sleep(2) #repeat two every seconds
 
 except KeyboardInterrupt:
      sys.exit
