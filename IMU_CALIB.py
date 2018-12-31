a4f9e2e562b10e055559815c31ef0f6a80b53245
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2018-12-31 13:24:49 -0600

    Changed classmethod to regular method that uses self argument

--- IMU_CALIB.py
+++ IMU_CALIB.py
@@ -3,13 +3,19 @@ import math
 class IMU_CALIB:
     def __init__(self):
         pass
-    @classmethod
-    def pitch_roll(cls, accelx, accely, accelz): # method to get pitch roll from raw accelerometer values
+    #@classmethod
+    def pitch(self, accelx, accely, accelz): # method to get pitch from raw accelerometer values
         
-        cls.accXnorm = accelx/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
-        cls.accYnorm = accely/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
+        self.accXnorm = accelx/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
+        self.calcpitch = math.asin(self.accXnorm)
 
-        cls.pitch = math.asin(cls.accXnorm)
-        cls.roll = -1 * math.asin(cls.accYnorm/math.cos(cls.pitch))
+        return(self.calcpitch)
 
-        #return(cls.pitch, cls.roll)
+    #@classmethod
+    def roll(self, accelx, accely, accelz): # method to get roll from raw accelerometer values
+        self.accXnorm = accelx/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
+        self.accYnorm = accely/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel y values
+
+        self.calcroll = -1 * math.asin(self.accYnorm/math.cos(self.pitch))
+
+        return(self.calcroll)
