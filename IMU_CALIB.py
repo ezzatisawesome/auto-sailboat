ca320b8887e193445d90273ce000b4ed895dd22c
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2018-12-31 13:10:53 -0600

    edited out return function in IMU_CALIB - test

--- IMU_CALIB.py
+++ IMU_CALIB.py
@@ -12,4 +12,4 @@ class IMU_CALIB:
         cls.pitch = math.asin(cls.accXnorm)
         cls.roll = -1 * math.asin(cls.accYnorm/math.cos(cls.pitch))
 
-        return(cls.pitch, cls.roll)
+        #return(cls.pitch, cls.roll)
