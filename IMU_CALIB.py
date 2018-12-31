9b8289052b393dd24ca94de8c61cfca26d1d857b
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2018-12-30 23:28:15 -0600

    Worked on IMU_CALIB for normalizing accelerometer data

--- IMU_CALIB.py
+++ IMU_CALIB.py
@@ -1,2 +1,14 @@
 import math
-#import magrawvalues (trying to import .csv file in MPU9250)
+
+class IMU_CALIB:
+    
+    @classmethod
+    def pitch_roll(cls, accelx, accely, accelz): # method to get pitch roll from raw accelerometer values
+        
+        cls.accXnorm = accelx/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
+        cls.accYnorm = accely/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
+
+        cls.pitch = math.asin(cls.accXnorm)
+        cls.roll = -1 * math.asin(cls.accYnorm/math.cos(cls.pitch))
+
+        return(cls.pitch, cls.roll)
---
+++ test.py
@@ -0,0 +1,32 @@
+import IMU_CALIB
+import MPU9250
+import sys
+import time
+
+mpu9250 = MPU9250.MPU9250()
+IMU_CALIB = IMU_CALIB.IMU_CALIB()
+
+try:
+     while True:
+        mag = mpu9250.readMagnet()
+        accel = mpu9250.readAccel()
+        
+        magx = mag['x'] # x mag value
+        magy = mag['y'] # y mag value
+        magz = mag['z'] # z mag value
+
+        accelx = accel['x'] # x mag value
+        accely = accel['y'] # y mag value
+        accelz = accel['z'] # z mag value
+
+        IMU_CALIB.pitch_roll(accelx, accely, accelz)
+
+        print(IMU_CALIB.pitch_roll)
+        print(IMU_CALIB.pitch_roll)
+
+        #print("{:>.3f}  {:>.3f}  {:>.3f}" .format(x, y, z))
+        
+        time.sleep(1) # repeat every second
+
+except KeyboardInterrupt:
+     sys.exit
