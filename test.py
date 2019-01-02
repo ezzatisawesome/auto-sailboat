6ed16137cacb3e3250cd78f45e002198066440bd
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2019-01-01 21:53:30 -0600

    Added in calibration values

--- test.py
+++ test.py
@@ -8,26 +8,37 @@ IMU_CALIB = IMU_CALIB.IMU_CALIB()
 
 try:
      while True:
-        mag = mpu9250.readMagnet() #read magnetometer
-        accel = mpu9250.readAccel() #read accelerometer
-        
-        magx = mag['x'] #x mag value
-        magy = mag['y'] #y mag value
-        magz = mag['z'] #z mag value
+          mag = mpu9250.readMagnet() #read magnetometer
+          accel = mpu9250.readAccel() #read accelerometer
+          
+          magx = mag['x'] #x mag value
+          magy = mag['y'] #y mag value
+          magz = mag['z'] #z mag value
+          
+          accelx = accel['x'] #x mag value
+          accely = accel['y'] #y mag value
+          accelz = accel['z'] #z mag value
+          
+          pitch = IMU_CALIB.pitch(accelx, accely, accelz) #creating instance of pitch method in IMU_CALIB lib
+          roll = IMU_CALIB.roll(accelx, accely, accelz) #creating instance of roll method in IMU_CALIB lib
+          
+          comp_heading = IMU_CALIB.mag_tilt_comp(magx, magy, magz) #creating instance of tilt compensation method in IMU_CALIB lib
+          
+          magXmax = 70.741
+          magXmin = -26.081
+          magYmax = 77.029
+          magYmin = -15.875
+          magZmax = 58.573
+          magZmin = -40.431
 
-        accelx = accel['x'] #x mag value
-        accely = accel['y'] #y mag value
-        accelz = accel['z'] #z mag value
+          calibration = IMU_CALIB.mag_calibration(magx, magy, magz, magXmax, magXmin, magYmax, magYmin, magZmax, magZmin)
 
-        pitch = IMU_CALIB.pitch(accelx, accely, accelz) #creating instance of pitch method in IMU_CALIB lib
-        roll = IMU_CALIB.roll(accelx, accely, accelz) #creating instance of roll method in IMU_CALIB lib
+          print(calibration)
 
-        comp_heading = IMU_CALIB.mag_tilt_comp(magx, magy, magz) #creating instance of tilt compensation method in IMU_CALIB lib
-
-        #print("{:>.4f} {:>.4f}" .format(pitch, roll))
-        print("{:>.4f}" .format(comp_heading))
-
-        time.sleep(.5) #repeat two every seconds
+          #print("{:>.4f} {:>.4f}" .format(pitch, roll))
+          # print("{:>.4f}" .format(comp_heading))
+           
+          # time.sleep(.5) #repeat two every seconds
 
 except KeyboardInterrupt:
      sys.exit
