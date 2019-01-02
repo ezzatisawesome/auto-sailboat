ed7a7805f15ce288b870320e84d48425d7095c7a
Ezzat Suhaime <meesdef@Ezzats-MBP.hsd1.il.comcast.net>
2019-01-01 21:06:20 -0600

    Worked on calibration library

--- IMU_CALIB.py
+++ IMU_CALIB.py
@@ -4,6 +4,7 @@ class IMU_CALIB:
     def __init__(self):
         pass
 
+
     def pitch(self, accelx, accely, accelz): #method to get pitch from raw accelerometer values
 
         accXnorm = accelx/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel x values
@@ -11,12 +12,36 @@ class IMU_CALIB:
 
         return(self.calcpitch)
 
+
     def roll(self, accelx, accely, accelz): #method to get roll from raw accelerometer values
 
         accYnorm = accely/(math.sqrt(accelx*accelx + accely*accely + accelz*accelz)) # normalize accel y values
         self.calcroll = -1*math.asin(accYnorm/math.cos(self.calcpitch)) #converting to roll values in radians
 
         return(self.calcroll)
+
+
+    def mag_calibration(self, magx, magy, magz, magXmax, magXmin, magYmax, magYmin, magZmax, magZmin): #to apply calibration
+        
+        offset_x = (magXmax + magXmin) / 2
+        offset_y = (magYmax + magYmin) / 2
+        offset_z = (magZmax + magZmin) / 2
+
+        avg_delta_x = (magXmax - magXmin) / 2
+        avg_delta_y = (magYmax - magYmin) / 2
+        avg_delta_z = (magZmax - magZmin) / 2
+
+        avg_delta = (avg_delta_x + avg_delta_y + avg_delta_z) / 3
+
+        scale_x = avg_delta / avg_delta_x
+        scale_y = avg_delta / avg_delta_y
+        scale_z = avg_delta / avg_delta_z
+
+        self.corrected_x = (magx - offset_x) * scale_x
+        self.corrected_y = (magy - offset_y) * scale_y
+        self.corrected_z = (magz - offset_z) * scale_z
+
+        return(self.corrected_x, self.corrected_y, self.corrected_z)
 
 
     def mag_tilt_comp(self, magx, magy, magz):
@@ -30,8 +55,8 @@ class IMU_CALIB:
             self.comp_heading += 360
 
         return(self.comp_heading)
-
  
+
         #sudo code
     #magXcomp = mag_raw[0]*cos(pitch)+mag_raw[2]*sin(pitch);
     #magYcomp = mag_raw[o]*sin(roll)*sin(pitch)+mag_raw[1]*cos(roll)-mag_raw[2]*sin(roll)*cos(pitch);
