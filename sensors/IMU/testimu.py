4203cd0f4fcd5ef9b7d0207ac4e3d1c06ff7d3e6
ezzatisawesome <ezzatisawesome@gmail.com>
2019-06-10 19:25:07 -0500

    edit

--- sensors/IMU/testimu.py
+++ sensors/IMU/testimu.py
@@ -1,15 +1,23 @@
 import IMU
 import sys
 import time
+import IMU_CALIB
 
 MPU9250 = IMU.MPU9250()
-
+calib = IMU_CALIB.IMU_CALIB()
+mag = MPU9250.readMagnet()
+accel = MPU9250.readAccel()
+north = calib.mag_tilt_comp()
 try:
     while True:
-        mag = MPU9250.readMagnet()
-        print(" mx = " , ( mag['x'] ))
-        print(" my = " , ( mag['y'] ))
-        print(" mz = " , ( mag['z'] ))
+        
+        mx = mag['x']
+        my = mag['y']
+        mz = mag['z']
+        ax = accel['x']
+        ay = accel['y']
+        az = accel['z']
+        print(north(mx, my, mz, ax, ay, az))
         time.sleep(1)
 except KeyboardInterrupt:
     sys.exit()
